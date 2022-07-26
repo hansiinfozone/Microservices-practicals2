9a5f477128cfa594db723b49d7af14165ef79959
hansiinfozone <109414703+hansiinfozone@users.noreply.github.com>
2022-07-27 00:14:28 +0530

    micro services

---
+++ 18-circuit breaker/rent-service/src/main/java/lk/codelabs/rentcloud/rentservice/RentServiceApplication.java
@@ -0,0 +1,16 @@
+package lk.codelabs.rentcloud.rentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan(basePackages = "lk.codelabs.rentcloud.model.rent")
+@EnableCircuitBreaker
+public class RentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentServiceApplication.class, args);
+    }
+
+}

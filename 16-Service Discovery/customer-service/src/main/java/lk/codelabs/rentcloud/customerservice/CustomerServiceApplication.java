9cb42b8d3d05f66135f4c87216fe70fb64753876
hansiinfozone <109414703+hansiinfozone@users.noreply.github.com>
2022-07-26 22:40:21 +0530

    micro services practcals

---
+++ 16-Service Discovery/customer-service/src/main/java/lk/codelabs/rentcloud/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,16 @@
+package lk.codelabs.rentcloud.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan(basePackages = "lk.codelabs.rentcloud.model.customer")
+@EnableEurekaClient
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+}
